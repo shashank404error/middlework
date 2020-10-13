a21f02b54552cc5cb35dad6c99d3e490e6afa0bd
Shashank Prakash <shashanksharma191098@gmail.com>
2020-10-13 11:38:32 +0530

    shashankMongo imported

--- middlework.go
+++ middlework.go
@@ -2,25 +2,10 @@ package middlework
 
 import(
 	"fmt"
+	"github.com/shashank404error/shashankMongo"
 )
 
-type ConnectToDataBase struct {
-	CustomApplyURI string 
-	DatabaseName string 
-	CollectionName string 
-}
-
-type ProfileConfig struct{
-	Zone int64 `bson: "zone" json: "zone"`
-	MessagePlan int64 `bson: "messageplan" json: "messageplan"`
-	Tracking bool `bson: "tracking" json: "tracking"`
-	ZoneID []string `bson: "zoneid" json: "zoneid"`
-}
-
-var dBConnect *ConnectToDataBase
-var config *ProfileConfig
-
-func CreateZones(dBConnect *ConnectToDataBase, userId string, config *ProfileConfig) {
+func CreateZones(dBConnect *shashankMongo.ConnectToDataBase, userId string, config *shashankMongo.ProfileConfig) {
 	for _,v := range config.ZoneID {
 		fmt.Println(v)
 	}
