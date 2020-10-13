cca6d5e5f632fb3e9a20de82957c95767611a84d
Shashank Prakash <shashanksharma191098@gmail.com>
2020-10-13 11:58:22 +0530

    insertone function added

--- middlework.go
+++ middlework.go
@@ -1,12 +1,30 @@
 package middlework
 
 import(
-	"fmt"
+	"log"
+	"encoding/json"
 	"github.com/shashank404error/shashankMongo"
 )
 
 func CreateZones(dBConnect *shashankMongo.ConnectToDataBase, userId string, config *shashankMongo.ProfileConfig) {
 	for _,v := range config.ZoneID {
-		fmt.Println(v)
+		
+		load:=`{
+			"name":"`+v+`",
+			"businessUid": "`+userId+`",
+			"noofparkingslots": 0
+			}`
+		loadToJson:=byteToJsonInterface(load)
+		_=shashankMongo.InsertOne(dBConnect,loadToJson)
 	}
+}
+
+func byteToJsonInterface(load string) map[string]interface{} {
+	var loadArr = []byte(load)
+    var loadToJson map[string]interface{}
+    err := json.Unmarshal(loadArr, &loadToJson)
+    if (err != nil) {
+		log.Fatal(err)
+	}
+	return loadToJson
 }
