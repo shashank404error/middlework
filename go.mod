3602d19bc8858b4e0b10b1fbab3d2c59a45249f4
Shashank Prakash <shashanksharma191098@gmail.com>
2020-10-14 13:50:37 +0530

    excel reader func

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/shashank404error/middlework
+
+go 1.12
+
+require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1
+	github.com/shashank404error/shashankMongo v0.0.0-20201014053234-67e677328f33
+	go.mongodb.org/mongo-driver v1.4.2 // indirect
+)
--- middlework.go
+++ middlework.go
@@ -2,8 +2,11 @@ package middlework
 
 import(
 	"log"
+	"io"
+	"fmt"
 	"encoding/json"
 	"github.com/shashank404error/shashankMongo"
+	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
 func CreateZones(dBConnect *shashankMongo.ConnectToDataBase,collectionName string, userId string, config *shashankMongo.ProfileConfig) {
@@ -18,6 +21,32 @@ func CreateZones(dBConnect *shashankMongo.ConnectToDataBase,collectionName strin
 		_=shashankMongo.InsertOne(dBConnect,collectionName,loadToJson)
 	}
 }
+
+func UploadToExcel(file io.Reader,dBConnect *shashankMongo.ConnectToDataBase,collectionName string, userId string) {
+
+	//fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+    //fmt.Printf("File Size: %+v\n", handler.Size)
+	//fmt.Printf("MIME Header: %+v\n", handler.Header)
+	f, err := excelize.OpenReader(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	rows := f.GetRows("Sheet1")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	for _, row := range rows {
+		fmt.Println(row[0])
+		fmt.Println(row[1])
+		fmt.Println(row[2])
+		fmt.Println(row[3])
+		fmt.Println(row[4])
+		fmt.Println("\n")
+	}
+}
+
 
 func byteToJsonInterface(load string) map[string]interface{} {
 	var loadArr = []byte(load)
